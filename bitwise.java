bb38b2dc9290a481fcc6550784470d5911aea9a3
nathi-2004 <s.nathisha13@gmail.com>
2025-02-12 12:50:39 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,17 @@
+class bitwise
+{
+    public static void main(String args[])
+    {
+        int a=492;
+        int b=333;
+        int c=a&b;
+        int d=a|b;
+        int e=a^b;
+        
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        
+        
+    }
+}
