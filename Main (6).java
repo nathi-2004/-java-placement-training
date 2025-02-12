2428ab709e231dd7b57743585e5f8b546b4078ff
nathi-2004 <s.nathisha13@gmail.com>
2025-02-12 16:05:30 +0530

    Add files via upload

---
+++ Main (6).java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main {
+    public static void main (String[] args) {
+       int a=7;
+       for(;;)
+       {
+           if(a>3)
+           System.out.println(a);
+           else 
+                 break;
+                a--;
+       }
+    }
+}
