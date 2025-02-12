63c11c3da28242885b397c7fafc2e9fcaa9d5b5a
nathi-2004 <s.nathisha13@gmail.com>
2025-02-12 14:32:06 +0530

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,15 @@
+class Main{
+    public static void main (String[] args) {
+    {
+        int x=10;
+       int  v=--x + x++ + ++x + --x + x++;
+    int z=x++ + ++x + ++x + x++ + ++x;
+    int a=--x + --x + --x + --x;
+    System.out.println(v);
+     System.out.println(z);
+     System.out.println(a);
+    
+    }
+}
+
+}
