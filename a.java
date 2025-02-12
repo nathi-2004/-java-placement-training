0f348423a0caa4288d229be348d7e8c760017067
nathi-2004 <s.nathisha13@gmail.com>
2025-02-12 11:59:30 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,26 @@
+class a
+{
+    public static void main(String args[])
+    {
+        int a=50;
+        int b=30;
+        int c=10;
+        //using relational operator
+       System.out.println(a==b);
+       System.out.println(a!=b);
+       System.out.println(a>=b);
+       System.out.println(a<=b);
+       System.out.println(a>b);
+       System.out.println(a<b);
+       // using shortcircuit operator
+       if((a>b)&&(a>c))
+       {
+           System.out.println("a is greatest");
+       }
+       // using non circuit operator
+       if(a>b||a>c)
+       {
+            System.out.println("a is greatest");
+       }
+}
+}
