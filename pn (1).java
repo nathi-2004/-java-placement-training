c5c85f3fc7b149d422c79d5516f4fe8662a0556b
nathi-2004 <s.nathisha13@gmail.com>
2025-02-12 15:59:39 +0530

    Add files via upload

---
+++ pn (1).java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+class pn {
+	public static void main (String[] args) {
+		{
+			int a=86;
+			int b=94;
+			if(true)
+			{
+				System.out.println("a"+"b");
+			}
+			else {
+				System.out.println(a+b);
+			}
+			System.out.println("a"+b);
+		}
+	}
+}
