ece9bc266669dd238c4f12f3a48207a88738ca08
nathi-2004 <s.nathisha13@gmail.com>
2025-02-12 11:32:34 +0530

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,8 @@
+class public static void main (String[] args) {
+    {
+        public static void main(String ars[])
+        {
+            System.out.println("HELLO WORLD");
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main
+    {
+public static void main(String args[])
+{
+    byte a=100;
+    byte b=98;
+    byte c=(byte)(a+b);
+    System.out.println("sum:"+ c);
+    byte d=(byte)(a-b);
+    System.out.println("Difference:"+ d);
+    byte e=(byte)(a*b);
+    System.out.println("Multiplication:"+ e);
+    byte f=(byte)(a/b);
+    System.out.println("Division:"+ f);
+    }
+    }
