01ba07052798062136e4bede506b39738b00745f
nathi-2004 <s.nathisha13@gmail.com>
2025-02-12 16:09:37 +0530

    Add files via upload

---
+++ Main (7).java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main {
+    public static void main (String[] args) {
+       int i=1;
+       while(i>0)
+       {
+           i=1;
+           System.out.println(i--);
+           i--;
+       }
+    }
+}
