a703591f7b4d42ce4e76c65b20026e6760be057e
nathi-2004 <s.nathisha13@gmail.com>
2025-02-13 11:49:29 +0530

    Add files via upload

--- Main (3).java
+++ Main (3).java
@@ -1,24 +1,17 @@
 import java.util.*;
-class Main{
-    public static void main (String[] args) {
-    {
-        for(int n=1;n<=100;n++)
-        {
-        int count=0;
-        for(int i=1;i<=n;i++)
-        {
-            if(n%i==0)
-            {
-                count++;
-            }
-        }
-        if(count==2)
-        {
-            System.out.println(n +"  is a prime number");
-            
-        }
-    }
-    }
+class Main {
+	public static void main (String[] args) {
+		{
+		    int n=5;
+		    for(int i=1;i<=n;i++)
+		    {
+		        for(int j=1;j<=i;j++)
+		        {
+		            System.out.print(i);
+		        }
+		        System.out.println();
+		    }
+		}
+	}
 }
-}
-        
+	
--- Main (4).java
+++ Main (4).java
@@ -1,18 +1,17 @@
 import java.util.*;
 class Main {
-    public static void main (String[] args) {
-        for (int n = 1; n <= 100; n++) {
-            int count = 0;
-            int i = 1;
-            while (i <= n) {
-                if (n % i == 0) {
-                    count++;
-                }
-                i++;
-            }
-            if (count == 2) {
-                System.out.println(n + " is a prime number");
-            }
-        }
-    }
+	public static void main (String[] args) {
+		{
+		    int n=5;
+		    for(int i=1;i<=n;i++)
+		    {
+		        for(int j=1;j<=i;j++)
+		        {
+		            System.out.print(j);
+		        }
+		        System.out.println();
+		    }
+		}
+	}
 }
+	
