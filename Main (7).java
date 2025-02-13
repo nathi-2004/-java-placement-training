1152bebfa153a37a9ddb0dda8ed1a1ca2c387ecf
nathi-2004 <s.nathisha13@gmail.com>
2025-02-13 12:27:03 +0530

    Add files via upload

--- Main (7).java
+++ Main (7).java
@@ -1,12 +1,27 @@
 import java.util.*;
 class Main {
-    public static void main (String[] args) {
-       int i=1;
-       while(i>0)
-       {
-           i=1;
-           System.out.println(i--);
-           i--;
-       }
-    }
+	public static void main (String[] args) {
+		{
+		    int n=5;
+		  
+		    for(int i=1;i<=n;i++)
+		    {
+		        for(int j=n-1;i<=j;j--)
+		        {
+		            System.out.print(" ");
+		          
+		        }
+		        for(int k=1;k<=i;k++)
+		        {
+		            System.out.print("1");
+		        }
+		        for(int a=1;a<i;a++)
+		        {
+		            System.out.print("1");
+		        }
+		        System.out.println();
+		    
+		}
+	}
+}
 }
