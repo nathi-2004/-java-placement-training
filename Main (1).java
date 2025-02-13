e5ca78249dc6663133903d90f44ba435fcbb7980
nathi-2004 <s.nathisha13@gmail.com>
2025-02-13 10:28:41 +0530

    Add files via upload

--- Main (1).java
+++ Main (1).java
@@ -1,8 +1,31 @@
-class public static void main (String[] args) {
-    {
-        public static void main(String ars[])
-        {
-            System.out.println("HELLO WORLD");
-        }
-    }
+import java.util.*;
+class Main {
+	public static void main (String[] args) {
+		{
+			Scanner in=new Scanner(System.in);
+			int n=in.nextInt();
+			int sum=0;
+			while(n>0)//123456
+			{
+				int digit=n%10;
+				n=n/10;//12345
+				if(digit%2==1)
+				{
+				sum+=digit;
+					System.out.println(digit);
+
+				}
+				
+
+			}
+			System.out.println(sum);
+		}
+	}
 }
+
+
+
+
+
+
+
