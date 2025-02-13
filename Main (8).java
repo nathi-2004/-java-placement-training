b93b97d675f711ba7831dac02e326603951cf9c1
nathi-2004 <s.nathisha13@gmail.com>
2025-02-13 12:30:34 +0530

    Add files via upload

--- Main (8).java
+++ Main (8).java
@@ -1,26 +1,31 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-       
-        Scanner in= new Scanner(System.in);
-
-        
-        System.out.print("Enter a number: ");
-        int n= in.nextInt();
-        int rev= 0;
-
-        
-        while (n!= 0) {
-            int digit = n % 10; 
-            n= n / 10;  
-            rev= rev* 10 + digit;
-            
-        }
-
-       
-        System.out.println(rev);
-
-       
-    }
+import java.util.*;
+class Main {
+	public static void main (String[] args) {
+		{
+		    int n=5;
+		  
+		    for(int i=1;i<=n;i++)
+		    {
+		        for(int j=n-1;i<=j;j--)
+		        {
+		            System.out.print(" ");
+		          
+		        }
+		        for(int k=1;k<=i;k++)
+		        {
+		            System.out.print("1");
+		        }
+		        for(int a=1;a<i;a++)
+		        {
+		            System.out.print("1");
+		        }
+		        System.out.println();
+		        /*for(k=1;k<=(2*i-1;k++)
+		        {
+		            sop(1)
+		        }*/
+		    
+		}
+	}
+}
 }
