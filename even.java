457da8ab0ea17126b5e02ef721d1f0f5dab74fb3
nathi-2004 <s.nathisha13@gmail.com>
2025-02-16 21:28:49 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,22 @@
+class even
+{
+	public static void main(String args[])
+	{
+		int n=123456;
+		int sum=0;
+		int digit;
+
+		while(n>0)
+		{
+			digit=n%10;
+			n=n/10;
+
+			if(digit%2!=0)
+			{
+				System.out.println(digit);
+				sum+=digit;
+			}
+		}
+		System.out.println(sum);
+	}
+}
