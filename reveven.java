9c9178821014d967aefd09274591a6872783eb26
nathi-2004 <s.nathisha13@gmail.com>
2025-02-16 22:58:24 +0530

    Add files via upload

---
+++ reveven.java
@@ -0,0 +1,21 @@
+class   reveven
+{
+    public static void main(String args[]) {
+        int n = 123456;
+        int sum = 0;
+        int value = 1; 
+        int digit;
+
+        while (n > 0) {
+            digit = n % 10; 
+            n = n / 10;   
+            if (digit % 2 == 0) { 
+                System.out.println(digit);
+                sum = sum + (digit * value); 
+                value *= 10; 
+            }
+        }
+
+        System.out.println("Final Sum (even digits as a number): " + sum);
+    }
+}
