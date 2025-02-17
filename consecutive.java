f0de2af802f858389d6bbf83043fd88614bee193
nathi-2004 <s.nathisha13@gmail.com>
2025-02-17 11:04:49 +0530

    Add files via upload

---
+++ consecutive.java
@@ -0,0 +1,21 @@
+class consecutive
+{
+    public static void main(String[] args) {
+        int c = 0;
+        int prev = 0; 
+        int arr[] = {1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0};
+
+       
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                c++; 
+            } else {
+                prev = Math.max(prev, c); 
+                c = 0; 
+            }
+        }
+    
+
+         System.out.println( prev);
+    }
+}
