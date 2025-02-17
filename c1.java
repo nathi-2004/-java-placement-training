d689b3745f372dfe9f9975fda214d5d8c91a7f5a
nathi-2004 <s.nathisha13@gmail.com>
2025-02-17 11:32:35 +0530

    Add files via upload

---
+++ c1.java
@@ -0,0 +1,18 @@
+class c1
+{
+	public static void main(String[] args) {
+	String input = "1101101101";
+	StringBuilder output = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+
+			if (i < 6) {
+				output.append('1');
+			} else {
+				output.append('0');
+			}
+		}
+
+       	System.out.println("Input: " + input);
+		System.out.println("Output: " + output.toString());
+	}
+}
