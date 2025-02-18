fa39049073019d4bcd88796270773a02e4cf2ce0
nathi-2004 <s.nathisha13@gmail.com>
2025-02-18 16:13:31 +0530

    Add files via upload

---
+++ index1.java
@@ -0,0 +1,22 @@
+class index1 {
+	public static void main(String args[])
+	{
+		int a[]= {2,7,5,7};
+		int n=9;
+		int count=0;
+		for(int i=0; i<a.length; i++)
+		{
+			for (int j = i + 1; j <a.length; j++)
+			{
+
+				if(a[i]+a[j]==n)
+				{
+					System.out.println(i + "," + j);
+			            count++;
+				}
+			
+			}
+		}
+		System.out.println(count);
+	}
+}
