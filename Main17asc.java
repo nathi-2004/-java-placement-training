15a9de1f2f63e14fe194d6686ba523540f1aa1f3
nathi-2004 <s.nathisha13@gmail.com>
2025-02-19 12:54:52 +0530

    Add files via upload

---
+++ Main17asc.java
@@ -0,0 +1,51 @@
+class Main17asc{
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {3, 2, 1},
+            {4, 9, 6},
+            {7, 5, 8}
+        };
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[] c = new int[rows * cols];
+        int k = 0;
+
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                c[k++] = matrix[i][j];
+            }
+        }
+
+       
+        for (int i = 0; i < c.length - 1; i++) {
+            for (int j = 0; j < c.length - 1 - i; j++) {
+                if (c[j] > c[j + 1]) {
+                  
+                    int temp = c[j];
+                    c[j] = c[j + 1];
+                    c[j + 1] = temp;
+                }
+            }
+        }
+
+       
+        k = 0; 
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = c[k++];
+            }
+        }
+
+        
+        System.out.println("Sorted Matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
