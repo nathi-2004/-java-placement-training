0e67ae1ee9e89a925afe3239813463ae4f4ad0f9
nathi-2004 <s.nathisha13@gmail.com>
2025-02-20 16:08:28 +0530

    Add files via upload

---
+++ Main (25).java
@@ -0,0 +1,25 @@
+class  book {
+    String title;
+    String author;
+    int price;
+book(String title,String author,int price)
+{
+    this.title=title;
+    this.author=author;
+    this.price=price;
+}
+     void display(){
+         System.out.println("BOOK TITLE : "+title);
+         System.out.println("AUTHOR : "+author);
+         System.out.println("Book PRICE : "+price);
+     }
+}
+class Main{
+    public static void main (String[] args) {
+    {
+        book  bookclass =new book("u can win","yashwant",500);
+         bookclass.display();
+        
+    }
+}
+}
