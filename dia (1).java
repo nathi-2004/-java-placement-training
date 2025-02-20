e6ee037614957939e6756fcc29f8a61fc6b33045
nathi-2004 <s.nathisha13@gmail.com>
2025-02-20 06:52:46 +0530

    Add files via upload

--- dia (1).java
+++ dia (1).java
@@ -1,33 +1,30 @@
-class dia{
+
+ class dia {
     public static void main(String[] args) {
         int[][] matrix = {
             {1, 2, 3},
             {4, 5, 6},
-            {7, 8, 9} };
+            {7, 8, 9}
+        };
 
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int upperTriangleSum = 0;
-        for (int i = 0; i < rows; i++) {
-            for (int j = i + 1; j < cols; j++) {
-                upperTriangleSum += matrix[i][j];
-            }
-        }
-        int lowerTriangleSum = 0;
-        for (int i = 1; i < rows; i++) {
-            for (int j = 0; j < i; j++) {
-                lowerTriangleSum += matrix[i][j];
-            }
-        }
-        matrix[0][0] = upperTriangleSum;  
+        int n = matrix.length; 
+
        
-        matrix[2][2] = lowerTriangleSum;  
-        System.out.println("Updated Matrix:");
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
+        for (int i = 0; i < n; i++) {
+            if (i < n - 1 - i) {  
+                int temp = matrix[i][n - 1 - i];  
+                matrix[i][n - 1 - i] = matrix[n - 1 - i][i];  
+                matrix[n - 1 - i][i] = temp;
+            }
+        }
+
+       
+        System.out.println("Matrix after reversing the diagonal:");
+        for (int i = 0; i < n; i++) { 
+            for (int j = 0; j < n; j++) { 
                 System.out.print(matrix[i][j] + " ");
             }
-            System.out.println();
+            System.out.println();  
         }
     }
 }
