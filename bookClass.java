02a4c3859c1c3601ba1a23c19328cbdca4bd5b29
nathi-2004 <s.nathisha13@gmail.com>
2025-02-20 15:08:54 +0530

    Add files via upload

---
+++ bookClass.java
@@ -0,0 +1,24 @@
+class bookClass
+{
+    String booktitle;
+    double bookprice;
+    bookClass()
+    {
+        booktitle="Java Programming";
+        bookprice=100.0;
+    }
+    void display()
+    {
+        System.out.println("BOOK TILE :"+ booktitle);
+         System.out.println("BOOK PRICE :"+ bookprice);
+        
+    }
+    
+    public static void main(String args[])
+    {
+    bookClass a = new bookClass();
+  
+    a.display();
+}
+
+}
