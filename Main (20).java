86ce44ca7ab11dbb0e05992f933e4aa86c815b3d
nathi-2004 <s.nathisha13@gmail.com>
2025-02-20 10:58:22 +0530

    Add files via upload

---
+++ Main (20).java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Main {
+
+    String name;
+    int regno;
+
+    void get(String n, int r) {
+        name = n;
+        regno = r;
+    }
+
+    void display() {
+        System.out.println(name);
+        System.out.println(regno);
+    }
+
+    public static void main(String args[]) {
+        Main[] students = new Main[5];  
+       
+        for (int i = 0; i < 5; i++) {
+            students[i] = new Main(); 
+        }
+
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Enter name of student " );
+            String name = in.nextLine();
+            System.out.println("Enter registration number of student ");
+            int regno = in.nextInt();
+            in.nextLine();
+            students[i].get(name, regno);
+        }
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Details of student ");
+            students[i].display();
+        }
+
+       
+    }
+}
