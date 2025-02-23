03e99480866762b4b19dff1fe87e5501a3bc0552
nathi-2004 <s.nathisha13@gmail.com>
2025-02-23 22:19:07 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class   Pattern{
+	public static void main (String[] args) {
+		{
+		    int n=4;
+		    for(int i=0;i<=n;i++)
+		    
+		    {
+		      
+		        for(int j=0;j<=i;j++)
+		        {
+		            System.out.print("1 ");
+		        }
+		        System.out.println();
+		    }
+		}
+	}
+}
+			
