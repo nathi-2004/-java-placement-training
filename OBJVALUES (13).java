29f90c5f6ec0bf56ce7d843fa004803a18374c58
nathi-2004 <s.nathisha13@gmail.com>
2025-02-23 23:07:15 +0530

    Add files via upload

---
+++ OBJVALUES (13).java
@@ -0,0 +1,24 @@
+class OBJVALUES{
+    
+   
+    static class Inner {
+        static int n = 5;
+
+        static void display() {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j <= i; j++) {
+                    if (j % 2 == 0) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print("#");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+       OBJVALUES  .Inner.display();
+    }
+}
