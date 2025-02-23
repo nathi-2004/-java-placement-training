69f2b201bf70ba2a6d96552a60c8a12bf60c6a07
nathi-2004 <s.nathisha13@gmail.com>
2025-02-23 22:33:45 +0530

    Add files via upload

---
+++ OBJVALUES (5).java
@@ -0,0 +1,24 @@
+class Car {
+	String brand;
+
+	Car() {
+		brand = "unknown";
+	}
+
+	void setBrand(String newBrand) {
+		brand = newBrand;
+	}
+
+	void display() {
+		System.out.println("Car Brand: " + brand);
+	}
+}
+class OBJVALUES{
+
+	public static void main(String[] args) {
+		Car a = new Car();
+		a.display();
+		a.setBrand("Toyota");
+		a.display();
+	}
+}
