87758e366667074757cdaa3a962e3878b48325ea
nathi-2004 <s.nathisha13@gmail.com>
2025-02-24 12:07:54 +0530

    Add files via upload

---
+++ ternary.java
@@ -0,0 +1,13 @@
+class ternary
+{
+    public static void main(String args[])
+    {
+        int a=15;
+        int b=30;
+        int c=10;
+        int max=((a>b)?a:b);
+        System.out.println(max);
+        int d=(a>b&&a>c)?a:(b>c)?b:c;
+        System.out.println(d);
+    }
+}
