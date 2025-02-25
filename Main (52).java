b31f2bc95faecf2d754626311038259890ecfb96
nathi-2004 <s.nathisha13@gmail.com>
2025-02-25 10:32:32 +0530

    Add files via upload

---
+++ Main (52).java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+      
+        int[] arr = {10, 4, 3, 50, 90, 90}; 
+        int[] result = new int[arr.length];
+        int resultIndex = 0; 
+        if (arr.length > 0) {
+           
+            int currentMax = arr[0];
+
+           
+            result[resultIndex++] = currentMax;
+
+          
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i] > currentMax) { 
+                    result[resultIndex++] = arr[i];  
+                    currentMax = arr[i]; 
+                }
+            }
+        }
+
+     for (int i = 0; i < resultIndex; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+}
