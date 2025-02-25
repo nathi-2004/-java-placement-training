790cbf93399d385a07dd1268eaa49cfc753a38f0
nathi-2004 <s.nathisha13@gmail.com>
2025-02-25 11:39:09 +0530

    Add files via upload

---
+++ Main (55).java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+class Main{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        for(;;){
+        int a=in.nextInt();
+       
+        if(a==42)
+        {
+            break;
+        }
+            System.out.println(a);
+        }
+       
+        }
+}
+
+     
+       
+           
+       
