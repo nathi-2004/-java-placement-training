8b58781659794c109ef8be79299657d9337d67d9
nathi-2004 <s.nathisha13@gmail.com>
2025-02-25 10:19:41 +0530

    Add files via upload

---
+++ InterfaceClass (1).java
@@ -0,0 +1,20 @@
+
+    interface a1{
+        void test1();
+    }
+    interface a2{
+        void test1();
+    }
+    class interfaceEx implements a1,a2{
+        public void test1()
+        {
+            System.out.println("nathi");
+     }
+    }
+    public class InterfaceClass{
+    public static void main(String args[])
+    {
+        interfaceEx obj=new interfaceEx();
+        obj.test1();
+    }
+}
