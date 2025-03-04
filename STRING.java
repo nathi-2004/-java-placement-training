c4aaaddc0187a6e227a4d69edb2c920ca794b211
nathi-2004 <s.nathisha13@gmail.com>
2025-03-04 11:11:04 +0530

    Add files via upload

---
+++ STRING.java
@@ -0,0 +1,71 @@
+class Main{
+    void string(){
+        String a="$aba$";
+        String rev = "";
+        for (int i = a.length() - 1; i >= 0; i--) {
+            rev=rev+ a.charAt(i); 
+        }
+
+        System.out.println("Reversed String: " + rev);
+        if(a.equals(rev)){
+            System.out.println("palindrome");
+        }
+        else{
+            System.out.println("not palindrome");
+        }
+    }
+     void stringbuilder(){
+        String b = "Hello123$#@";
+        StringBuilder sb = new StringBuilder(b);
+        System.out.println("Reversed String: " + sb.reverse());
+        if(b.equals(sb.reverse())){
+            System.out.println("palindrome");
+        }
+        else{
+            System.out.println("not palindrome");
+        }
+    }
+    
+     void stringbuffer(){
+         String c = "@#a#@";
+        StringBuffer sb = new StringBuffer(c);
+        System.out.println("Reversed String: " + sb.reverse());
+         if(c.equals(sb.reverse().toString())){
+            System.out.println("palindrome");
+        }
+        else{
+            System.out.println("not palindrome");
+        }
+    }
+    
+  void withoutstring() {
+        String d= "13a$$a31";
+        char[] charArray = d.toCharArray();
+        for (int i = 0, j = charArray.length - 1; i < j; i++, j--) 
+        {
+            char temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+        }
+        
+        System.out.println("Reversed String: " + new String(charArray));
+        if(d.equals(new String(charArray)))
+        {
+              System.out.println("palindrome");
+            
+        } else{
+            System.out.println("not palindrome");
+        }
+    
+}
+
+     
+    public static void main(String args[]){
+        Main obj=new Main();
+        obj.string();
+         obj.stringbuilder();
+         obj.stringbuffer();
+         obj.withoutstring();
+       
+    }
+}
